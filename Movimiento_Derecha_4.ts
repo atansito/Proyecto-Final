30d5a3f6c0d754f2442865c87ba7e8d7cee39e82
atansito <141583527+atansito@users.noreply.github.com>
2024-04-22 19:40:27 -0600

    Update pxt.json, main.blocks, main.ts, Movimiento_Derecha_1.ts, Movimiento_Derecha_2.ts, Movimiento_Derecha_3.ts, Movimiento_Derecha_4.ts

---
+++ Movimiento_Derecha_4.ts
@@ -0,0 +1,24 @@
+// Add your code here
+const Movimiento_Derecha_4 = img`
+    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 f f f f f f f f 0 0 0 0 0
+    0 0 0 f f f f f f f f f f 0 0 0 0
+    0 0 f f f f f f f f f f f f 0 0 0
+    0 f f f f f f f f f f f f f f 0 0
+    0 f f f f f f d d d d f f f f f 0
+    0 f f f f d d d d d d d d f f 0 0
+    0 f f f d d d d d d d d d f 0 0 0
+    0 f f f d d d f d d d f d f 0 0 0
+    0 0 f d d d d f d d d f d f 0 0 0
+    0 0 f d d d d d d d d d d f 0 0 0
+    0 0 0 f d d d d d d d d f 0 0 0 0
+    0 0 0 0 f f b f d b f f 0 0 0 0 0
+    0 0 0 0 f f b b f b b f 0 0 0 0 0
+    0 0 0 0 f f d d f b b f 0 0 0 0 0
+    0 0 0 0 f c f f c c c f 0 0 0 0 0
+    0 0 0 0 f c f f f f c f 0 0 0 0 0
+    0 0 0 0 f c f 0 0 0 f c f 0 0 0 0
+    0 0 0 f c f 0 0 0 0 0 f c f 0 0 0
+    0 0 0 f f f 0 0 0 0 0 f f f 0 0 0
+    `;
--- main.ts
+++ main.ts
@@ -1,4 +1,107 @@
-let Principal = sprites.create(img`
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    Principal,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `],
+    200,
+    true
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    animation.runImageAnimation(
+    Principal,
+    [
+    Quieto_Derecho_1,
+    Quieto_Derecho_2,
+    Quieto_Derecho_3,
+    Quieto_Derecho_4
+    ],
+    200,
+    true
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    Principal,
+    [
+    Movimiento_Derecha_1,
+    Movimiento_Derecha_2,
+    Movimiento_Derecha_3,
+    Movimiento_Derecha_4
+    ],
+    200,
+    true
+    )
+})
+let Principal: Sprite = null
+Principal = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -18,6 +121,8 @@ let Principal = sprites.create(img`
     `, SpriteKind.Player)
 Principal.setPosition(20, 150)
 scene.cameraFollowSprite(Principal)
+controller.moveSprite(Principal)
+Principal.ay += 300
 animation.runImageAnimation(
 Principal,
 [
