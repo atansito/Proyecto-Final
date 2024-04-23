a24cbcd4f62195c5df083b149b28367eca96ae1d
atansito <141583527+atansito@users.noreply.github.com>
2024-04-22 19:46:05 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, Movimiento_Izquierdo_1.ts, Movimiento_Izquierdo_2.ts, Movimiento_Izquierdo_3.ts, Movimiento_Izquierdo_4.ts, Quieto_Izquierdo_1.ts, Quieto_Izquierdo_2.ts, Quieto_Izquierdo_3.ts, Quieto_Izquierdo_4.ts

---
+++ Movimiento_Izquierdo_4.ts
@@ -0,0 +1,24 @@
+// Add your code here
+const Movimiento_Izquierdo_4 = img`
+    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 f f f f f f f f 0 0 0 0
+    0 0 0 0 f f f f f f f f f f 0 0 0
+    0 0 0 f f f f f f f f f f f f 0 0
+    0 0 f f f f f f f f f f f f f f 0
+    0 f f f f f d d d d f f f f f f 0
+    0 0 f f d d d d d d d d f f f f 0
+    0 0 0 f d d d d d d d d d f f f 0
+    0 0 0 f d f d d d f d d d f f f 0
+    0 0 0 f d f d d d f d d d d f 0 0
+    0 0 0 f d d d d d d d d d d f 0 0
+    0 0 0 0 f d d d d d d d d f 0 0 0
+    0 0 0 0 0 f f b d f b f f 0 0 0 0
+    0 0 0 0 0 f b b f b b f f 0 0 0 0
+    0 0 0 0 0 f b b f d d f f 0 0 0 0
+    0 0 0 0 0 f c c c f f c f 0 0 0 0
+    0 0 0 0 0 f c f f f f c f 0 0 0 0
+    0 0 0 0 f c f 0 0 0 f c f 0 0 0 0
+    0 0 0 f c f 0 0 0 0 0 f c f 0 0 0
+    0 0 0 f f f 0 0 0 0 0 f f f 0 0 0
+    `;
---
+++ Quieto_Izquierdo_3.ts
@@ -0,0 +1,24 @@
+// Add your code here
+const Quieto_Izquierdo_3 = img`
+    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 f f f f f f f f 0 0 0 0
+    0 0 0 0 f f f f f f f f f f 0 0 0
+    0 0 0 f f f f f f f f f f f f 0 0
+    0 0 f f f f f f f f f f f f f f 0
+    0 f f f f f d d d d f f f f f f 0
+    0 0 f f d d d d d d d d f f f f 0
+    0 0 0 f d d d d d d d d d f f f 0
+    0 0 0 f d f d d d f d d d f f f 0
+    0 0 0 f d f d d d f d d d d f 0 0
+    0 0 0 f d d d d d d d d d d f 0 0
+    0 0 0 0 f d d d d d d d d f 0 0 0
+    0 0 0 0 0 f f b d d b f f 0 0 0 0
+    0 0 0 0 f f b b e e b b f f 0 0 0
+    0 0 0 f b f b b e e b b b b f 0 0
+    0 0 0 f d f b b c c b f d d f 0 0
+    0 0 0 0 f f c f f f f c f f 0 0 0
+    0 0 0 0 0 f c f 0 0 f c f 0 0 0 0
+    0 0 0 0 0 f c f 0 0 f c f 0 0 0 0
+    0 0 0 0 0 f f f 0 0 f f f 0 0 0 0
+    `;
