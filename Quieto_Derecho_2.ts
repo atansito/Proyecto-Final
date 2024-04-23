179ee3f45482dc79d09d1253d22941f6a6b3f53e
atansito <141583527+atansito@users.noreply.github.com>
2024-04-22 19:16:12 -0600

    Initial files for MakeCode project

---
+++ Quieto_Derecho_2.ts
@@ -0,0 +1,24 @@
+// Add your code here
+const Quieto_Derecho_2 = img`
+    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 f f f f f f f f 0 0 0 0 0
+    0 0 0 f f f f f f f f f f 0 0 0 0
+    0 0 f f f f f f f f f f f f 0 0 0
+    0 f f f f f f f f f f f f f f 0 0
+    0 f f f f f f d d d d f f f f f 0
+    0 f f f f d d d d d d d d f f 0 0
+    0 f f f d d d d d d d d d f 0 0 0
+    0 f f f d d d f d d d f d f 0 0 0
+    0 0 f d d d d f d d d f d f 0 0 0
+    0 0 f d d d d d d d d d d f 0 0 0
+    0 0 0 f d d d d d d d d f 0 0 0 0
+    0 0 0 f f f b d d b f f f 0 0 0 0
+    0 0 f b b b b e e b b f b f 0 0 0
+    0 0 f d d f b e e b b f d f 0 0 0
+    0 0 0 f f c c c c c c f f 0 0 0 0
+    0 0 0 0 f c f f f f c f 0 0 0 0 0
+    0 0 0 0 f c f 0 0 f c f 0 0 0 0 0
+    0 0 0 0 f c f 0 0 f c f 0 0 0 0 0
+    0 0 0 0 f f f 0 0 f f f 0 0 0 0 0
+    `;
---
+++ Quieto_Derecho_3.ts
@@ -0,0 +1,24 @@
+// Add your code here
+const Quieto_Derecho_3 = img`
+    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 f f f f f f f f 0 0 0 0 0
+    0 0 0 f f f f f f f f f f 0 0 0 0
+    0 0 f f f f f f f f f f f f 0 0 0
+    0 f f f f f f f f f f f f f f 0 0
+    0 f f f f f f d d d d f f f f f 0
+    0 f f f f d d d d d d d d f f 0 0
+    0 f f f d d d d d d d d d f 0 0 0
+    0 f f f d d d f d d d f d f 0 0 0
+    0 0 f d d d d f d d d f d f 0 0 0
+    0 0 f d d d d d d d d d d f 0 0 0
+    0 0 0 f d d d d d d d d f 0 0 0 0
+    0 0 0 0 f f b d d b f f 0 0 0 0 0
+    0 0 0 f f b b e e b b f f 0 0 0 0
+    0 0 f b b b b e e b b f b f 0 0 0
+    0 0 f d d f b c c b b f d f 0 0 0
+    0 0 0 f f c f f f f c f f 0 0 0 0
+    0 0 0 0 f c f 0 0 f c f 0 0 0 0 0
+    0 0 0 0 f c f 0 0 f c f 0 0 0 0 0
+    0 0 0 0 f f f 0 0 f f f 0 0 0 0 0
+    `;
